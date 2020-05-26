0491bd3aa9d99b7557f7cfc2422894d3a11d79a3
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-05-26 23:19:36 +0530

    implementation completed

--- build/bcryptHashing.d.ts
+++ build/bcryptHashing.d.ts
@@ -1,2 +1,22 @@
-export declare class BcryptHashing {
+import { IHasher } from "@rheas/contracts/security";
+export declare class BcryptHashing implements IHasher {
+    /**
+     * The number of rounds to be used for salt generation.
+     *
+     * @var number
+     */
+    protected rounds: number;
+    /**
+     * @inheritdoc
+     *
+     * @param value
+     */
+    createHash(value: string): Promise<string>;
+    /**
+     * @inheritdoc
+     *
+     * @param value
+     * @param hashedValue
+     */
+    compare(value: string, hashedValue: string): Promise<boolean>;
 }
--- src/bcryptHashing.ts
+++ src/bcryptHashing.ts
@@ -1,3 +1,32 @@
-export class BcryptHashing {
+import bcrypt from "bcrypt";
+import { IHasher } from "@rheas/contracts/security";
+
+export class BcryptHashing implements IHasher {
+
+    /**
+     * The number of rounds to be used for salt generation.
+     * 
+     * @var number
+     */
+    protected rounds: number = 10;
+
+    /**
+     * @inheritdoc
+     * 
+     * @param value 
+     */
+    public async createHash(value: string): Promise<string> {
+        return await bcrypt.hash(value, this.rounds);
+    }
+
+    /**
+     * @inheritdoc
+     * 
+     * @param value 
+     * @param hashedValue 
+     */
+    public async compare(value: string, hashedValue: string): Promise<boolean> {
+        return await bcrypt.compare(value, hashedValue);
+    }
 
 }
